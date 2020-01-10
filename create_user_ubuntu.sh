43a285018a1c1461bf98a2070477dfa3b6ead254
AaG7xNnrgbzeyqc5woPS <47978362+AaG7xNnrgbzeyqc5woPS@users.noreply.github.com>
2020-01-10 20:24:32 +0800

    Update create_user_ubuntu.sh

--- create_user_ubuntu.sh
+++ create_user_ubuntu.sh
@@ -20,8 +20,8 @@ apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
 apt-get clean
-apt-get -y automove
-apt-get -y upgrade
+apt-get automove
+#apt-get -y upgrade
 
 # Only fro learning
 # userdel user  #olny delete user
